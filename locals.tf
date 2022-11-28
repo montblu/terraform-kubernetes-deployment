7ac0a233b60bd9310d21f508a4c39847759de15d
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-11-28 20:37:50 +0000

    locals.tf: Add one more condition when the image is not set by the user

--- locals.tf
+++ locals.tf
@@ -8,5 +8,5 @@ locals {
 
   labels = var.labels == {} ? local.default_labels : merge(local.default_labels, var.labels)
 
-  image = var.ecr_create ? aws_ecr_repository.main.repository_url : "dummy"
+  image = var.image == "" ? (var.ecr_create ? aws_ecr_repository.main.repository_url : "dummy") : var.image
 }
