75f0a3f73ea34abf584dd99ec06513a6a4511931
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-11-28 20:35:26 +0000

    locals.tf: Add image

--- locals.tf
+++ locals.tf
@@ -7,4 +7,6 @@ locals {
   }
 
   labels = var.labels == {} ? local.default_labels : merge(local.default_labels, var.labels)
+
+  image = var.ecr_create ? aws_ecr_repository.main.repository_url : "dummy"
 }
