33784c1a1cc3fe89a027d3132b43cefae42e4b5c
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-11-28 19:20:15 +0000

    locals.tf: Add labels

--- locals.tf
+++ locals.tf
@@ -1,3 +1,8 @@
 locals {
   resource_name = "${var.name_prefix}-${var.name}"
+  default_labels = {
+    app = local.resource_name
+  }
+
+  labels = var.labels == {} ? local.default_labels : merge(local.default_labels, var.labels)
 }
