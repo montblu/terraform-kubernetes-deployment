2f6bd055b1d78309a56cbec4849d30266f828619
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-12-01 02:02:14 +0000

    deployment.tf: Add liveness_probe

--- deployment.tf
+++ deployment.tf
@@ -56,7 +56,45 @@ resource "kubernetes_deployment" "main" {
           name              = local.resource_name
           image             = local.image
           image_pull_policy = var.image_pull_policy
-          working_dir       = var.working_dir
+          dynamic "liveness_probe" {
+            for_each = var.liveness_probe
+            content {
+              dynamic "exec" {
+                for_each = lookup(liveness_probe.value, "exec", [])
+                content {
+                  command = lookup(exec.value, "name", null)
+                }
+              }
+              failure_threshold = lookup(liveness_probe.value, "failure_threshold", [])
+              dynamic "http_get" {
+                for_each = lookup(liveness_probe.value, "http_get", [])
+                content {
+                  host = lookup(http_get.value, "host", null)
+                  dynamic "http_header" {
+                    for_each = lookup(http_get.value, "http_header", [])
+                    content {
+                      name  = lookup(http_header.value, "name", null)
+                      value = lookup(http_header.value, "value", null)
+                    }
+                  }
+                  path   = lookup(http_get.value, "path", null)
+                  port   = lookup(http_get.value, "port", null)
+                  scheme = lookup(http_get.value, "scheme", null)
+                }
+              }
+              initial_delay_seconds = lookup(liveness_probe.value, "initial_delay_seconds", null)
+              period_seconds        = lookup(liveness_probe.value, "period_seconds", null)
+              success_threshold     = lookup(liveness_probe.value, "success_threshold", null)
+              dynamic "tcp_socket" {
+                for_each = lookup(liveness_probe.value, "tcp_socket", [])
+                content {
+                  port = lookup(tcp_socket.value, "port", null)
+                }
+              }
+              timeout_seconds = lookup(liveness_probe.value, "timeout_seconds", null)
+            }
+          }
+          working_dir = var.working_dir
 
           resources {
             limits   = var.resource_limits
