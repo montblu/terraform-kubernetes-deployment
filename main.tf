9690bab85ecd9b2ac2e64e1b0089d65fd3b26a56
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-12-01 23:43:37 +0000

    main.tf: Pass locals to here

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+locals {
+  resource_name = "${var.name_prefix}-${var.name}"
+
+  annotations = var.annotations
+  default_labels = {
+    app = local.resource_name
+  }
+
+  labels = var.labels == {} ? local.default_labels : merge(local.default_labels, var.labels)
+
+  image = var.image == "" ? (var.ecr_create ? aws_ecr_repository.main.repository_url : "dummy") : var.image
+}
+
+
 ################################################################################
 # ECR Repository
 ################################################################################
