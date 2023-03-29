cb5ab23e9548a2802ae9f108b4757928b6dc3613
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2023-03-29 10:55:26 +0100

    main.tf: Add rolling_update block

--- main.tf
+++ main.tf
@@ -133,6 +133,13 @@ resource "kubernetes_deployment" "main" {
     replicas = var.replicas
     strategy {
       type = var.strategy_type
+
+      dynamic "rolling_update" {
+        for_each = var.strategy_type == "RollingUpdate" ? strategy_rolling_update : []
+
+        max_surge       = lookup(rolling_update.value, "max_surge", "")
+        max_unavailable = lookup(rolling_update.value, "max_unavailable", "")
+      }
     }
 
     selector {
