f2b6a01325887737e379793b04e98caf7beb2a12
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2023-03-29 15:33:31 +0100

    main.tf: Fix missing variable call

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "kubernetes_deployment" "main" {
       type = var.strategy_type
 
       dynamic "rolling_update" {
-        for_each = var.strategy_type == "RollingUpdate" ? strategy_rolling_update : []
+        for_each = var.strategy_type == "RollingUpdate" ? var.strategy_rolling_update : []
 
         max_surge       = lookup(rolling_update.value, "max_surge", "")
         max_unavailable = lookup(rolling_update.value, "max_unavailable", "")
