15f4e84448bdfd0578f534aa1c8db08ef9c4fe22
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2024-12-27 19:06:54 +0000

    outputs.tf: Add deployment_name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ecr_repository_url" {
   description = "The URL of the ECR repository"
   value       = try(aws_ecr_repository.main[0].repository_url, "")
 }
+
+output "deployment_name" {
+  description = "The name of the deployment"
+  value       = try(kubernetes_deployment.main[0].metadata[0].name, "")
+}
