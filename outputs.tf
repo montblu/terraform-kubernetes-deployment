c4e4d6cc85d65d0bef363ea856d9b1edf8401f95
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2024-12-27 19:31:11 +0000

    outputs.tf: Add svc_name, svc_ports, svc_address

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,18 @@ output "deployment_annotations" {
   description = "The annotations of the deployment"
   value       = try(kubernetes_deployment.main[0].metadata[0].annotations, {})
 }
+
+output "svc_name" {
+  description = "The name of the service"
+  value       = try(kubernetes_service.main[0].metadata[0].name, "")
+}
+
+output "svc_ports" {
+  description = "The port(s) of the service"
+  value       = try(kubernetes_service.main[0].spec[0].port, [])
+}
+
+output "svc_address" {
+  description = "The address of the service within the cluster"
+  value       = "${kubernetes_service.main[0].metadata[0].name}.${kubernetes_service.main[0].metadata[0].namespace}.svc.cluster.local"
+}
